8f3987775b49afcc112528e77537a2a3fcb94d75
Thiago Thomas <thiago_rocha17@hotmail.com>
2019-04-15 08:18:45 -0300

    Interface para o usuario

--- src/Principal.java
+++ src/Principal.java
@@ -1,15 +1,87 @@
+import java.util.Scanner;
+
 public class Principal {
 
-	public static void main(String[] args) {
+	//chamarElevador() << chama o elevador
+	//entrarElevador() << entra no elevador
+	//escolherAndar(int and) << escolhe o andar
+	//sairElevador() << sai do elevador
+	//alguemPegouElev() << alguem pega o Elevador
+	//status() << status do elevador
+	
+	public static void main(String[] args){
 		
-		Elevador e1 = new Elevador();
-		e1.status();
-		e1.chamarElevador();
-		e1.entrarElevador();
-		e1.escolherAndar(10);
-		e1.sairElevador();
-		e1.status();
-
+		Scanner leia = new Scanner(System.in);
+		
+		Elevador elevator = new Elevador();
+		
+		int opcao = 0;
+		
+		System.out.println("BEM VINDO AO SIMULADOR DE ELEVADOR!!!");
+		
+		do {
+			
+			System.out.println("DIGITE 1 PARA CHAMAR O ELEVADOR");
+			System.out.println("DIGITE 2 PARA ENTRAR NO ELEVADOR");
+			System.out.println("DIGITE 3 PARA SAIR DO ELEVADOR");
+			System.out.println("DIGITE 4 PARA SABER OS STATUS DO ELEVADOR");
+			System.out.println("DIGITE 5 PARA ESCOLHER O ANDAR QUE VOCE QUE IR");
+			System.out.println("DIGITE 6 PARA FECHAR!");	
+			
+			opcao = leia.nextInt();
+			
+			switch (opcao) {
+				
+				case 1:
+					System.out.println();
+					System.out.println("Aguarde enquanto o elevador chega ");
+					elevator.chamarElevador();
+					System.out.println();
+					break;
+				
+				case 2:
+					System.out.println();
+					elevator.entrarElevador();;
+					System.out.println();
+					break;
+					
+				case 3:
+					System.out.println();
+					elevator.sairElevador();
+					System.out.println();
+					break;
+					
+				case 4:
+					System.out.println();
+					elevator.status();
+					System.out.println();
+					break;
+					
+				case 5:
+					System.out.println();
+					if(elevator.isInsideElev()) {
+						System.out.print("Digite o Andar que voce quer: ");
+						int andar = leia.nextInt();
+						elevator.escolherAndar(andar);
+					}else {
+						System.out.println("ERRO! Voce precisa está dentro do elevador!!");
+					}							
+					System.out.println("");
+					break;
+					
+				case 6:
+					System.out.println("");
+					System.out.println("Saindo...");
+						
+				default: 
+					System.out.println("");
+					System.out.println("Numero invalido!!");
+					System.out.println("");
+				
+			}
+			
+		}while(opcao != 6 );
+		
 	}
 
 }
